f3b98467b6f3922273b1fc2d63612af5bfaf75d8
Muhammad Abdul Karim <alkariim.tridek@gmail.com>
2020-02-16 23:11:50 +0700

    change status & about

--- src/assets/colors/index.js
+++ src/assets/colors/index.js
@@ -5,11 +5,11 @@ export default {
     backgroudDefault: "#F4F4F4",
 
     status1: "#999999",
-    status2: "#28A745",
+    status2: "#6F42C1",
     status3: "#DC3545",
     status4: "#F39C12",
     status5: "#17A2B8",
-    status6: "#6F42C1",
+    status6: "#28A745",
     status7: "#343A40",
 
     white: "#FFFFFF",
--- src/screens/profile/AboutUs.js
+++ src/screens/profile/AboutUs.js
@@ -2,21 +2,30 @@ import React, { Component } from "react";
 import { Text, View, Image } from "react-native";
 import { Container } from "@app/components";
 import Styles from "@app/assets/styles";
-import { NavigationServices } from "@app/services";
+import { MarkdownView } from "react-native-markdown-view";
 
 export default class AboutUs extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            about: undefined,
         };
+    }
+
+    componentDidMount() {
+        this.setState({
+            about: "MD"
+        })
     }
 
     render() {
         return (
             <View style={Styles.containerDefault}>
                 <Container>
-                    <Text>AboutUs</Text>
+                    <MarkdownView>
+                        {this.state.about}
+                    </MarkdownView>
                 </Container>
             </View>
         );
